946e1bb3e7182cb62d8447af6ccb2df33ade7456
Bhavya Jain <63778065+bhavjainbj@users.noreply.github.com>
2022-01-21 18:44:04 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+  return(
+  <div>
+        This is about component
+        <p>This is todo App developed by Bhavya Jain</p>
+  </div>
+  )
+};
